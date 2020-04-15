15ec5bd15d87c3eb2820b9de314ecca1c76a20ef
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-15 12:52:48 -0400

    Updates

---
+++ precache-manifest.af77749c14f441a2cfbbac32c28e49e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f4363aa184ddc2d3579b96177d992a",
+    "url": "/videos/index.html"
+  },
+  {
+    "revision": "2d66f90030b7f3163dd6",
+    "url": "/videos/static/css/main.1d74ccc9.chunk.css"
+  },
+  {
+    "revision": "ca47c3b9da835d3456c3",
+    "url": "/videos/static/js/2.15c9c51f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videos/static/js/2.15c9c51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d66f90030b7f3163dd6",
+    "url": "/videos/static/js/main.9c9cd193.chunk.js"
+  },
+  {
+    "revision": "b253f015dc5b2482f6ec",
+    "url": "/videos/static/js/runtime-main.92da5185.js"
+  }
+]);
